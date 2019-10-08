e1ef15eefc4341768227202872b136746a708857
vitalina_p <39164339+vitalina777@users.noreply.github.com>
2019-10-08 13:46:43 +0300

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -34,7 +34,7 @@ xui.Class('App', 'xui.Module',{
                         "target":"msg",
                         "args":[
                             undefined,
-                            "{page.xui_ui_label50.getValue()}",
+                            "{page.xui_ui_label50.getName()}",
                             undefined,
                             5000
                         ],
